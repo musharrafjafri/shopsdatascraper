54c5608d3c79ed6802e30d65a3dd5c8bf732f64b
musharrafjafri <62510647+musharrafjafri@users.noreply.github.com>
2021-01-15 23:56:03 +0500

    modify main.py with line by line update .csv

--- main.py
+++ main.py
@@ -34,18 +34,22 @@ sleep(2)
 
 driver.find_element_by_class_name('wUrVib').click()
 print('On details page...')
-sleep(1)
-data_dict = defaultdict(list)
+sleep(3)
+# data_dict = defaultdict(list)
+# pd.DataFrame(data_dict).to_csv('test.csv', mode='a', header=False)
 def get_data_fun():
-    sleep(1)
+    my_dict = {
+    }
     for item_name in driver.find_elements_by_class_name('dbg0pd'):
-        data_dict['Name'].append(item_name.find_element_by_tag_name('div').text)
+        # data_dict['Name'].append(item_name.find_element_by_tag_name('div').text)
+        my_dict['Name'] = item_name.find_element_by_tag_name('div').text
         item_name.click()
-        sleep(1)
+        sleep(2)
         # name_flag = False
         # address_flag = False
         c_no_flag = False
         link_flag = False
+
 
         # Phone No extraction
         detail_cont = driver.find_element_by_class_name('ifM9O')
@@ -53,32 +57,38 @@ def get_data_fun():
             if span.get_attribute('role') == "link":
                 # phone_nums.append(span.text)
                 c_no_flag = True
-                data_dict['ContactNo'].append(span.text)
+                my_dict['ContactNo'] = span.text
+                # data_dict['ContactNo'].append(span.text)
         if c_no_flag == False:
-            data_dict['ContactNo'].append('Not found.')
+            my_dict['ContactNo'] = 'Not found'
+            # data_dict['ContactNo'].append('Not found.')
+
 
         # Address extraction
-        data_dict['Address'].append(driver.find_element_by_class_name('LrzXr').text)
+        my_dict['Address'] = driver.find_element_by_class_name('LrzXr').text
+        # data_dict['Address'].append(driver.find_element_by_class_name('LrzXr').text)
+
 
         #  Website links extractions
-        links_cont = driver.find_element_by_class_name('SPZz6b')
+        links_cont = driver.find_element_by_class_name('IzNS7c')
         for website_link in links_cont.find_elements_by_tag_name('a'):
             if website_link.find_element_by_tag_name('div').text == 'Website':
                 link_flag = True
-                data_dict['Link'].append(website_link.get_attribute('href'))
-                print(website_link.get_attribute('href'))
-
+                my_dict['Website'] = website_link.get_attribute('href')
+                # data_dict['Link'].append(website_link.get_attribute('href'))
         if link_flag == False:
-            data_dict['Link'].append('Not Found')
-
+            my_dict['Website'] = 'Not found'
+            # data_dict['Link'].append('Not Found')
+        # print(my_dict)
+    # print('THis is dict ', my_dict)
+        pd.DataFrame(my_dict, index=list('0')).to_csv(f'{s_items} in {s_location}.csv', mode='a', index=False, header=False)
     # return data_dict
-
-def to_csv_fun():
-    # data_dict = get_data_fun()
-    # c_time = time.ctime()[11:16]
-    table_data = pd.DataFrame(data_dict, index=range(1, (len(data_dict['Name']) + 1)))
-    table_data.to_csv(f'{s_items}_in_{s_location}.csv')
-    # print(f"File '{s_items}_in_{s_location}_{c_time}.csv' is generated...")
+# def to_csv_fun():
+#     # data_dict = get_data_fun()
+#     # c_time = time.ctime()[11:16]
+#     table_data = pd.DataFrame(data_dict, index=range(1, (len(data_dict['Name']) + 1)))
+#     table_data.to_csv(f'{s_items}_in_{s_location}.csv')
+#     # print(f"File '{s_items}_in_{s_location}_{c_time}.csv' is generated...")
 
 
 def main():
@@ -107,7 +117,7 @@ def main():
         else:
             next_but_flag = False
             # print("Check Point 3")
-    to_csv_fun()
+    # to_csv_fun()
 
 
 main()
--- prac.py
+++ prac.py
@@ -1,21 +1,32 @@
-# import pandas as pd
-# from _collections import defaultdict
-# import Files
-# my_dict = defaultdict(list)
-#
-# for i in range(5):
-#     my_dict['First'].append(i)
-#
-# for j in range(5):
-#     my_dict['Second'].append(j)
-#
-# print(len(my_dict['First']) + 1)
-# form = pd.DataFrame(my_dict, index=range(1, (len(my_dict['First']) + 1)))
-# print(form)
-#
-# # form.to_csv('test1.csv', index=False)
-flag = True
-if flag:
-    pass
-elif flag:
-    print('not ture')
+import pandas as pd
+from _collections import defaultdict
+
+
+my_dict = defaultdict(list)
+pd.DataFrame(my_dict, columns=['First', 'Second', 'Third']).to_csv('test.csv', mode='a')
+index_num = 0
+flage = True
+while flage:
+    for i in range(1, 6):
+        index_num += 1
+        my_dict['First_value'].append(i * 1000)
+        if i == 4:
+            flage = False
+    for i in range(1, 6):
+        my_dict['Second_value'].append(i * 1000)
+        if i == 4:
+            flage = False
+    for i in range(1, 6):
+        my_dict['Third_value'].append(i * 1000)
+        if i == 4:
+            flage = False
+    form = pd.DataFrame(my_dict, index=range(1, (len(my_dict['First_value']) + 1)))
+    form.to_csv('test.csv', mode='a', header=False)
+for i in range(len(my_dict['First_value'])):
+    print(i)
+print('Keys', len(my_dict['First_value']))
+# form = pd.DataFrame(my_dict, index=range(1, (len(my_dict['First_value']) + 1)))
+
+print(index_num)
+print(form)
+# greet.to_csv('test4.csv', mode='a', header=False)
